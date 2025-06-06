ee5dc52ac51efeb6149417e895fb6e9e71f19e74
script-premium <autoscriptvip@gmail.com>
2025-06-06 22:40:18 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "au3.pages.dev"; // Ganti dengan domain utama kalian
-const serviceName = "samudra"; // Ganti dengan nama workers kalian
+const serviceName = "samudra-"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
